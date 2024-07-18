1e543c285dc2bd4105fff828be28e70c085f2af7
grif0241 <grif0241@algonquinlive.com>
2024-07-18 17:17:40 -0400

    Improve logic

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// globals
+// global variables
 let playerSelection;
 let computerSelection;
 let gameCount = 0;
 let playerWins = 0;
 let computerWins = 0;
 
-game();
+playGame();
 
 // randomly generate cpu choice
-function getComputerChoice() {
-  let computerChoice;
+function getComputerChoice() { // String
+  let computerChoice = null;
   const randomNumber = Math.ceil(Math.random() * 3);
   switch (randomNumber) {
     case 1:
@@ -22,19 +22,32 @@ function getComputerChoice() {
       computerChoice = "Scissors";
       break;
     default:
-      computerChoice = "idk";
+      computerChoice = null;
   }
-  return computerChoice;
+  return computerChoice.toLowerCase();
+}
+
+// get humanChoice
+function getHumanChoice() { // String
+  const playerChoice = prompt("Choose rock, paper or scissors").toLowerCase();
+  if (playerChoice == "rock" || playerChoice == "scissors" || playerChoice == "paper") {
+    return playerChoice;
+  } else {
+    return null;
+  }
 }
 
 // eval function
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) { // String
   let outputMessage;
   let roundMessage;
+  gameCount = gameCount + 1;
 
+  // check if tie
   if (playerSelection == computerSelection) {
     roundMessage = `TIE! Both chose ${playerSelection}`;
   } else {
+    // switch on player selection
     switch (playerSelection) {
       case "rock":
         if (computerSelection == "scissors") {
@@ -70,27 +83,31 @@ function playRound(playerSelection, computerSelection) {
       }
     }
   }
+  // print output message after a round
   outputMessage = `Games played: ${gameCount}\n${roundMessage}\nPlayer: ${playerWins} vs Computer: ${computerWins}\n`;
   console.log(outputMessage);
   return outputMessage;
 }
 
 // point of entry function
-function game() {
+function playGame() { // Undefined
   while (gameCount < 5) {
-    playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
-    if (playerSelection == "rock" || playerSelection == "scissors" || playerSelection == "paper") {
-      gameCount++;
-      computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+
+    // check for valid values
+    if (playerSelection && computerSelection) {
       playRound(playerSelection, computerSelection);
+    } else {
+      playGame();
     }
   }
 
   // after 5 rounds:
   if (playerWins > computerWins) {
-    console.log("PLAYER WINS!");
+    console.log("PLAYER WINS THE MATCH!");
   } else if (computerWins > playerWins) {
-    console.log("COMPUTER WINS!");
+    console.log("COMPUTER WINS THE MATCH!");
   } else {
     console.log("IT'S A DRAW!");
   }
